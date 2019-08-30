fedead83146fd82007e282b529358eb8538eac4a
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-08-30 05:35:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -118,5 +118,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages]
 end
+
+    it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do
+      expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
